a6ee6e876d8dc722838cc855de2ffcad8354ca84
ArashAfsaryan <arashafsaryan@gmail.com>
2024-02-22 11:44:51 +0330

    new structures added to my project

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
+console.log("my name is arash afsaryan");
